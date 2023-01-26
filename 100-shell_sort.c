38bda74f329ee2c1bd637119c6547102b88bcf7c
Jose Sigawa <sigawajoseph1@gmail.com>
2023-01-26 15:51:09 +0300

    rectified

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,35 +1,46 @@
 #include "sort.h"
+#include <stdio.h>
 /**
- *shell_sort - function that sorts an array
- *
- *@array: array to sort
- *@size: number of elements of the array
+ * _swap - swaped 2 values.
+ * @array: the array for swap him values.
+ * @i: First index
+ * @j: Second index
+ * Return: Nothing
+ */
+void _swap(int *array, int i, int j)
+{
+	int tmp;
+
+	if (array[i] != array[j])
+	{
+		tmp = array[i];
+		array[i] = array[j];
+		array[j] = tmp;
+	}
+}
+
+/**
+ * shell_sort - sort the list and print the changes
+ * @array: The array to sort.
+ * @size: Size of the array
+ * Return: Nothing
  */
 void shell_sort(int *array, size_t size)
 {
-size_t aux = 1, i, flag;
-int temp;
+	size_t h = 0,  i, j;
 
-while (aux < size)
-aux = aux * 3 + 1;
-aux = (aux - 1) / 3;
-while (aux > 0)
-{
-flag = 0;
-for (i = 0; i + aux < size; i++)
-{
-if (array[i] > array[i + aux])
-{
-temp = array[i];
-array[i] = array[i + aux];
-array[i + aux] = temp;
-flag = 1;
-}
-}
-if (flag == 0)
-{
-aux = (aux - 1) / 3;
-print_array(array, size);
-}
-}
+	if (size < 2)
+		return;
+
+	while (h <= size / 3)
+		h = h * 3 + 1;
+
+	while (h >= 1)
+	{
+		for (i = h; i < size; i++)
+			for (j = i; j >= h && array[j] < array[j - h]; j -= h)
+				_swap(array, j, j - h);
+		h /= 3;
+		print_array(array, size);
+	}
 }
--- sort.h
+++ sort.h
@@ -1,9 +1,10 @@
-#ifndef _SORT_H
-#define _SORT_H
+#ifndef SORT_H
+#define SORT_H
 
-#include <stdio.h>
+
+#include <stddef.h>
 #include <stdlib.h>
-#include <string.h>
+
 
 /**
  * struct listint_s - Doubly linked list node
@@ -14,60 +15,26 @@
  */
 typedef struct listint_s
 {
-  const int n;
-  struct listint_s *prev;
-  struct listint_s *next;
+	const int n;
+	struct listint_s *prev;
+	struct listint_s *next;
 } listint_t;
 
-/**
- * enum kind_e - enum for the cards
- * @SPADE: group for spade cards
- * @HEART: group for heart cards
- * @CLUB: group for club cards
- * @DIAMOND: group for diamond cards
- */
-
-typedef enum kind_e
-  {
-    SPADE = 0,
-    HEART,
-    CLUB,
-    DIAMOND
-  } kind_t;
-
-/**
- * struct card_s - Playing card
- *
- * @value: Value of the card
- * From "Ace" to "King"
- * @kind: Kind of the card
- */
-typedef struct card_s
-{
-  const char *value;
-  const kind_t kind;
-} card_t;
-
-/**
- * struct deck_node_s - Deck of card
- *
- * @card: Pointer to the card of the node
- * @prev: Pointer to the previous node of the list
- * @next: Pointer to the next node of the list
- */
-typedef struct deck_node_s
-{
-  const card_t *card;
-  struct deck_node_s *prev;
-  struct deck_node_s *next;
-} deck_node_t;
+/* Print Functions */
 
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+
+
+/* Sorting Algorithms Functions */
+
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
+
+
+/*Advanced Tasks*/
 void shell_sort(int *array, size_t size);
 void cocktail_sort_list(listint_t **list);
 void counting_sort(int *array, size_t size);
@@ -76,6 +43,5 @@ void heap_sort(int *array, size_t size);
 void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
 
-#endif
+#endif /* SORT_H */
